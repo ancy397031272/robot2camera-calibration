839e3a7fd2c23ab914dd853ac82097f1b658f7f7
Michael Sobrepera <mjsobrep@live.com>
2016-03-21 14:43:29 -0400

    added license and manifest, updated setup

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+def license():
+    with open('license.rst') as f:
+        return f.read()
+
 setup(name='robot2cam_calibration',
       version='0.1',
-      description='A package to calibrate a UR CB2 Robot to a '
-                  'Flycapture2 Camera',
+      description='A package to calibrate a UR CB2 Robot to a camera',
       long_description=readme(),
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Manufacturing',
         'Intended Audience :: Science/Research',
-        'License :: Other/Proprietary License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
@@ -24,7 +27,7 @@ setup(name='robot2cam_calibration',
       url='-',
       author='Michael Sobrepera',
       author_email='mjsobrep@live.com',
-      license='Copyright (c) 2016 GTRC. All rights reserved.',
+      license=license(),
       packages=['robot2cam_calibration'],
       install_requires=[
           'numpy',
@@ -34,8 +37,9 @@ setup(name='robot2cam_calibration',
       ],
       include_package_data=True,
       entry_points={
-        'console_scripts': ['cb2-listen=ur_cb2.receive.cb2_receive_example:'
+        'console_scripts': ['robot2cam-record=ur_cb2.receive.cb2_receive_example:'
                             'main',
-                            'cb2-record=ur_cb2.receive.cb2_store_points:main']
+                            'robot2cam-calculate=ur_cb2.receive.cb2_store_points:main',
+                            'robot2cam-show=']
       },
       zip_safe=False)
