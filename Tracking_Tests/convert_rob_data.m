ed80642dc5707709d9e50713c1effbb202081b22
Michael <mjsobrep@live.com>
2016-04-27 14:12:59 -0400

    changed to jsonlab and changed to rotm

--- Tracking_Tests/convert_rob_data.m
+++ Tracking_Tests/convert_rob_data.m
@@ -2,17 +2,17 @@ function [ Rmatrices ] = convert_rob_data( filename )
 %Function which takes the correspondences.json file from robot 2 camera
 %calibration and returns the corresponding rotation matrices
 
+addpath('jsonlab/jsonlab/')
+
 fid=fopen(filename);
 scanned_file=fscanf(fid,'%s');
-parsed_json=parse_json(scanned_file);
-jstruct=parsed_json{1};
-tcp2robot=jstruct.tcp2robot;
+parsed_json=loadjson(scanned_file);
+tcp2robot=parsed_json.tcp2robot;
 Rmatrices=zeros(4,4,length(tcp2robot));
-for i=1:length(tcp2robot)
-    datacell=tcp2robot{i};
-    rmag=sqrt(datacell{4}^2+datacell{5}^2+datacell{6}^2);
-    rotation=quat2tform([rmag datacell{4} datacell{5} datacell{6}]);
-    translation=trvec2tform([datacell{1} datacell{2} datacell{3}])
+for i=1:size(tcp2robot,1)
+    rmag=sqrt(tcp2robot(i,4)^2+tcp2robot(i,5)^2+tcp2robot(i,6)^2);
+    rotation=rotm2tform(vrrotvec2mat([tcp2robot(i,4) tcp2robot(i,5) tcp2robot(i,6) rmag]));
+    translation=trvec2tform([tcp2robot(i,1) tcp2robot(i,2) tcp2robot(i,3)]);
     %Rmatrices(1:4,1:4,i)=translation*rotation;
     Rmatrices(1:4,1:4,i)=rotation*translation;
 end
