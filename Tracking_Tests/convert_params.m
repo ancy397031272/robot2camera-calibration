f1960dc2eef6d21bbfbc3265479544fd60384214
Michael <mjsobrep@live.com>
2016-05-02 13:04:37 -0400

    removed now outdated tracking_tests file

--- Tracking_Tests/convert_params.m
+++
@@ -1,6 +0,0 @@
-%Script to convert found transformation matrix from camera to robot base to
-%its inverse usable form
-Rotm=tform2rotm(TBase);
-Tvec=tform2trvec(TBase);
-invRotm=inv(Rotm)
-invTvec=-1*Tvec
